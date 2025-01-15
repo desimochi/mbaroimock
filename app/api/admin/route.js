266f40fe4acf13d3a71c99e68ba9f35b37e2eb2a
Puneet Sharma <puni199711@gmail.com>
2025-01-15 11:41:54 +0530

    user admin acess manager

---
+++ app/api/admin/route.js
@@ -0,0 +1,24 @@
+import clientPromise from "@/lib/mongodb";
+
+export async function GET() {
+
+  try {
+    const client = await clientPromise;
+    const db = client.db('sample_mflix');
+    
+    const users = await db.collection('users').find().toArray();
+    
+    return new Response(JSON.stringify(users), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+  } catch (err) {
+    console.error("Error fetching users", err);
+    return new Response(
+        JSON.stringify({ message: "Failed to fetch mock test details" }),
+        {
+          status: 500,
+          headers: { "Content-Type": "application/json" },
+        } )
+  }
+}
