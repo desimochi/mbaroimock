d21442bf350074e945a2ebdd3276f618cef85c2f
desimochi <desimochi@gmail.com>
2025-05-23 16:29:57 +0530

    paymentsssxsssss

--- components/PayButton.js
+++ components/PayButton.js
@@ -24,7 +24,7 @@ export default function PayButton() {
     const data = await res.json();
 
     if (!window.Razorpay) {
-      alert("Razorpay SDK not loaded.");
+      alert("Razorpay SDK not loadedd.");
       return;
     }
 
